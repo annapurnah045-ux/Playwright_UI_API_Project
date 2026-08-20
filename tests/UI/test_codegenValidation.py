06749dbf2100167de51d0ebd5cb6c8c7d09f47ff
Annapurna H <annapurnah045@gmail.com>
2026-08-20 17:57:45 +0530

    Temporarily disabled codegen test

--- tests/UI/test_codegenValidation.py
+++ tests/UI/test_codegenValidation.py
@@ -1,12 +1,22 @@
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page
 
 
-def run(playwright: Playwright) -> None:
+def test_playwright_test1(playwright):
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+    page = browser.new_page()
+    page.goto("https://google.com")
+    print(page.title())
     print("Disabled Codegen test temporarily")
+    browser.close()
+
+# def run(playwright: Playwright) -> None:
+#     browser = playwright.chromium.launch(headless=False)
+#     context = browser.new_context()
+#     page = context.new_page()
+
     # page.goto("https://rahulshettyacademy.com/client/#/auth/login")
     # page.get_by_role("textbox", name="email@example.com").click()
     # page.get_by_role("textbox", name="email@example.com").fill("")
@@ -72,5 +82,5 @@ def run(playwright: Playwright) -> None:
     # browser.close()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+# with sync_playwright() as playwright:
+#     run(playwright)
